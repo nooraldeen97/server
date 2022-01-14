d801c13e8088642b60d196b4c037bb474e39069c
nooraldeen97 <noor-aldeen97@hotmail.com>
2022-01-14 16:12:19 +0200

    finished lab13

---
+++ schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE IF NOT EXISTS movies (
+    id serial PRIMARY KEY,
+    title VARCHAR(255),
+    release_date VARCHAR(255),
+    poster_path VARCHAR(255),
+    overview VARCHAR(255),
+    comment VARCHAR(255)
+);
